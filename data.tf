9eab24fdcf79359339d7e88f70ca7a24de17c7e0
Apgaua <apgaua@protonmail.com>
2024-09-04 10:54:18 -0300

    Ajuste security groups

--- data.tf
+++ data.tf
@@ -1,7 +1,11 @@
+data "aws_vpc" "main" {
+  tags = {
+    "Name" = "containers-vpc"
+  }
+}
 data "aws_ssm_parameter" "vpc" {
   name = var.ssm_vpc_id
 }
-
 data "aws_ssm_parameter" "subnet_public_1" {
   name = var.ssm_public_subnet_1
 }
--- security_group.tf
+++ security_group.tf
@@ -1,5 +1,6 @@
-resource "aws_security_group" "main" {
-  name   = format("%s", var.project_name)
+#LB Security group
+resource "aws_security_group" "lb" {
+  name   = format("%s-load-balancer", var.project_name)
   vpc_id = data.aws_ssm_parameter.vpc.value
 
   egress {
@@ -10,14 +11,41 @@ resource "aws_security_group" "main" {
       "0.0.0.0/0"
     ]
   }
+  dynamic "ingress" {
+    for_each = local.lb_ports
+    content {
+    from_port = ingress.value
+    to_port   = ingress.value
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Libera trafego na porta ${ingress.value}"
+    }
+  }
 }
 
-resource "aws_security_group_rule" "subnet_ranges" {
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  type              = "ingress"
-  description       = "Trafego para a VPC"
-  cidr_blocks       = ["10.0.0.0/16"]
-  security_group_id = aws_security_group.main.id
+
+
+## Main security group
+resource "aws_security_group" "main" {
+  name   = format("%s-load-balancer-sg", var.project_name)
+  vpc_id = data.aws_ssm_parameter.vpc.value
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  dynamic "ingress" {
+    for_each = local.ecs_ports
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = "-1"
+      cidr_blocks = [ data.aws_vpc.main.cidr_block ]
+      description = "Libera trafego para vpc"
+      
+    }
+   
+  }
 }
